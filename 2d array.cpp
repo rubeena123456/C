4652aaf350d30122e678d6a0f24e450d3ec579a0
rubeena123456 <rubeena.2924@gmail.com>
2025-07-31 10:31:25 +0530

    Add files via upload

---
+++ 2d array.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a[2][3],i,j;
+	printf("enter elements\n");
+	for(i=0;i<2;i++)
+	for(j=0;j<3;j++)
+	scanf("%d",&a[i][j]);
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<3;j++)
+		printf("%d",a[i][j]);
+		printf("\n");
+		
+	}
+}
